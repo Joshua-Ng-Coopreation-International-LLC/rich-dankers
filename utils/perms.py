ea3aabb74295ea0065856598f66f2def8a5a0541
apsw171118 <ng_joshua@hotmail.com>
2024-11-03 20:49:10 +0000

    Update perms.py

--- utils/perms.py
+++ utils/perms.py
@@ -1,4 +1,4 @@
-from nextcord.ext import commands
+from nextcord.ext import commands, application_checks
 import json
 
 
@@ -39,12 +39,31 @@ def premium_command_handler(command: str = True):
     return commands.check(predicate)
 
 
-def ping_group_command_handler(command: str = True):
-    if command == True:
+def staff_role_converter(role_name: str):
+    try:
+        return (
+            load_config()["roles"]["staffroles"][role_name]
+            if role_name in load_config()["roles"]["staffroles"]
+            else load_config()["masterrole"]
+        )
+    except KeyError:
+        return load_config()["staffroles"]["botdeveloper"]
 
-        async def predicate(ctx):
-            list = (
-                load_config()["grouppingcommands"]["default"]
-                + load_config()["grouppingcommands"]["bypassall"]
-            )
-            return commands.has_any_role(*list)
+
+def ping_group_slash_permission_handler(value: str):
+    config = load_config()
+    ping_config = config["ping"]
+    roles_to_check = ping_config.get(value, [])
+    bypass_roles = ping_config.get("bypassall", [])
+    role_ids = []
+    for role in roles_to_check + bypass_roles:
+        if isinstance(role, str):
+            role_ids.extend(staff_role_converter(role))
+        else:
+            role_ids.append(role)
+
+    async def predicate(interaction):
+        user_roles = [role.id for role in interaction.user.roles]
+        return any(role_id in user_roles for role_id in role_ids)
+
+    return application_checks.check(predicate)
